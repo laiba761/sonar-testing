ba94d74b92594f875a532238ce01c5fb869b7ab7
laiba761 <123631114+laiba761@users.noreply.github.com>
2023-01-27 16:13:24 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# sonar-testing
+ sonar
---
+++ src/testing/Sonar_Testing.java
@@ -0,0 +1,20 @@
+package testing;
+
+/**
+ * TODO: Document me!
+ *
+ * @author laiba.mumtaz
+ *
+ */
+public class Sonar_Testing {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("cjw");
+        // TODO Auto-generated method stub
+
+    }
+
+}
