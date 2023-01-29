9c07abe2c9d64ae9d5f1af3f5f1ed07f449f2833
laiba761 <123631114+laiba761@users.noreply.github.com>
2023-01-29 12:06:43 +0500

    new

--- src/testing/Sonar_Testing.java
+++ src/testing/Sonar_Testing.java
@@ -6,12 +6,21 @@ package testing;
  * @author laiba.mumtaz
  *
  */
+import java.util.HashSet;
+
 public class Sonar_Testing {
 
     /**
      * @param args
      */
     public static void main(String[] args) {
+        HashSet<String> cars = new HashSet<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("BMW");
+        cars.add("Mazda");
+        System.out.println(cars);
         System.out.println("cjw");
         // TODO Auto-generated method stub
 
